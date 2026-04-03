b2180a8cbf7a420d7f926f0e1e4d57018987ceec
pangeding <3532341261@qq.com>
2026-04-03 17:58:49 +0800

    160

--- 1-160-link.go
+++ 1-160-link.go
@@ -1,1 +1,21 @@
 package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, World!")
+}
+
+func getIntersectionNode(headA, headB *ListNode) *ListNode {
+	// 1. 如果 A 或 B 为空 责退出
+	if headA == nil {
+
+	}
+
+	// 2. 
+}
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
