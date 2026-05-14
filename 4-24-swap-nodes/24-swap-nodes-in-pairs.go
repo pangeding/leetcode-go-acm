c8186c431d38f3f9070d018d7eb7caa22ba5dd45
pangeding <15533858+pangeding@user.noreply.gitee.com>
2026-05-14 10:30:45 +0800

    24: enter the nums

--- 4-24-swap-nodes/24-swap-nodes-in-pairs.go
+++ 4-24-swap-nodes/24-swap-nodes-in-pairs.go
@@ -10,6 +10,11 @@ func main() {
 	// only 大写 开头函数可以被外部类访问 go 语言中
 
 	var nums []int
+	for i := 0; i < n; i++ {
+		var val int
+		fmt.Scan(&val)
+		nums = append(nums, val)
+	}
 	
 	head := buildList(nums)
 	newHead := swapPairs(head)
