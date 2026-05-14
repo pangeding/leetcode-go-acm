2c56a37c8d8555715c63738ce2de4f3b7910a055
pangeding <15533858+pangeding@user.noreply.gitee.com>
2026-05-14 09:56:23 +0800

    24: scan the n

--- 4-24-swap-nodes/24-swap-nodes-in-pairs.go
+++ 4-24-swap-nodes/24-swap-nodes-in-pairs.go
@@ -1,12 +1,15 @@
 package swapNode
 
-import {
-
-}
+import (
+	"fmt"
+)
 
 func main() {
+	var n int
+	fmt.Scan(&n) // read the number of the node
+	// only 大写 开头函数可以被外部类访问 go 语言中
 
-	var nums int[]
+	var nums []int
 	
 	head := buildList(nums)
 	newHead := swapPairs(head)
