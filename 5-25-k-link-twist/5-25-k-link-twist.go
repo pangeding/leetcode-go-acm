3f99c45caef38614f2bf284e53b5bde0ed8f8889
pangeding <15533858+pangeding@user.noreply.gitee.com>
2026-05-20 11:43:41 +0800

    25: core: this false version, can run successfully, but it didn't reverse the List

---
+++ 5-25-k-link-twist/5-25-k-link-twist.go
@@ -0,0 +1,68 @@
+package main
+
+type ListNode struct {
+	Val int
+	Next *ListNode
+}
+
+func reverse(head *ListNode, tail *ListNode) *ListNode {
+	var prev *ListNode
+	cur := head
+	for cur != tail {
+		// the reverse logic
+		next := cur.Next
+		cur.Next = prev
+		// after the reverse, the position should be switched
+		prev = cur
+		cur = next
+	}
+
+	return prev
+}
+
+func reverseKGroup(head *ListNode, k int) *ListNode {
+	// head == nil or head.Next == nil or 
+	if head == nil || k <= 1 {
+		return head
+	}
+
+	// dummy
+	dummy := &ListNode{Next: head}
+	prev := dummy	
+
+	for head != nil {
+		tail := head
+		// reverse
+		// find the tail
+		for i := 0; i < k; i++ {
+			tail = tail.Next
+			if tail != nil {
+				return dummy.Next
+			}
+		}
+		// if fail to find the tail, return 
+
+		// if can find the tail, continue the reverse logic
+
+		// mark the next prev
+		nextHead := tail.Next
+
+		// reverse
+		newHead := reverse(head, tail)
+
+		// reconnect the linkList
+		prev.Next = newHead
+		head.Next = nextHead
+
+		// continue the next loop
+		prev = nextHead
+	}
+	
+
+	// return
+	return dummy.Next
+}
+
+func main() {
+	
+}
